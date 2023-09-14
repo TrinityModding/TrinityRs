d363e51bfaba64ece3dcd4c1cf0097c26bf486ab
Hayden Verschelden <haydenv06@gmail.com>
2023-09-14 16:16:15 +1000

    make shaders external

---
+++ shaders/vertex.glsl
@@ -0,0 +1,15 @@
+#version 450
+
+layout(location = 0) in vec2 position;
+layout(location = 0) out vec3 outColor;
+
+void main() {
+    const vec3 colors[3] = vec3[3](
+        vec3(1.0f, 0.0f, 0.0f), //red
+        vec3(0.0f, 1.0f, 0.0f), //green
+        vec3(00.f, 0.0f, 1.0f)  //blue
+    );
+
+    gl_Position = vec4(position, 0.0, 1.0);
+    outColor = colors[gl_VertexIndex];
+}
