a9b5cc23260116592840bdf3c6978ec26a4448d7
hYdos <haydenv06@gmail.com>
2023-09-25 14:23:40 +1000

    Move attribute code to utils

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,6 @@ use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
 use vulkano::pipeline::graphics::multisample::MultisampleState;
 use vulkano::pipeline::graphics::rasterization::{FrontFace, RasterizationState};
 use vulkano::pipeline::graphics::subpass::PipelineRenderingCreateInfo;
-use vulkano::pipeline::graphics::vertex_input::{VertexInputAttributeDescription, VertexInputBindingDescription, VertexInputRate, VertexInputState};
 use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
 use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
 use vulkano::render_pass::{AttachmentLoadOp, AttachmentStoreOp};
@@ -37,9 +36,9 @@ use winit::event::{Event, VirtualKeyCode, WindowEvent};
 use winit::event::ElementState::{Pressed, Released};
 use winit::event_loop::ControlFlow;
 
-use crate::io::model::{Attribute, AttributeSize, IndexLayout, SubMesh, MeshGroup, AttributeType, RGB32_FLOAT, RGBA16_FLOAT, RG32_FLOAT, RGBA8_UNORM, RGBA8_UNSIGNED, RGBA16_UNORM};
+use crate::io::model::{Attribute, IndexLayout, SubMesh, MeshGroup, AttributeType, RGB32_FLOAT, RGBA16_FLOAT, RG32_FLOAT, RGBA8_UNORM, RGBA8_UNSIGNED, RGBA16_UNORM};
 use crate::rendering::renderer::{Recorder, Renderer};
-use crate::rendering::renderer_utils::load_png;
+use crate::rendering::renderer_utils::{create_vertex_layout, load_png};
 
 mod rendering;
 mod io;
@@ -347,39 +346,8 @@ impl ModelRenderGraph {
         let mut uploaded_models = Vec::new();
 
         for mesh in model {
-            let mut vertex_attributes = Vec::new();
-            let mut vertex_bindings = Vec::new();
             let attribs = &mesh.attributes;
-            let total_size = calculate_element_size(attribs);
-            let mut offset = 0;
-            for attr_idx in 0..attribs.len() {
-                let attribute = attribs.get(attr_idx).unwrap();
-                let vk_type = match attribute.size {
-                    AttributeSize::None(_, _) => panic!("impossible"),
-                    AttributeSize::Rgba8UNorm(_, _) => Format::R8G8B8A8_UNORM,
-                    AttributeSize::Rgba8Unsigned(_, _) => Format::R8G8B8A8_UINT,
-                    AttributeSize::R32UInt(_, _) => Format::R32_UINT,
-                    AttributeSize::R32Int(_, _) => Format::R32_SINT,
-                    AttributeSize::Rgba16UNorm(_, _) => Format::R16G16B16A16_UNORM,
-                    AttributeSize::Rgba16Float(_, _) => Format::R16G16B16A16_SFLOAT,
-                    AttributeSize::Rg32Float(_, _) => Format::R32G32_SFLOAT,
-                    AttributeSize::Rgb32Float(_, _) => Format::R32G32B32_SFLOAT,
-                    AttributeSize::Rgba32Float(_, _) => Format::R32G32B32A32_SFLOAT,
-                };
-                let size = attribute.get_size();
-
-                vertex_attributes.push((attr_idx as u32, VertexInputAttributeDescription {
-                    binding: 0,
-                    format: vk_type,
-                    offset,
-                }));
-                vertex_bindings.push((0, VertexInputBindingDescription {
-                    stride: total_size,
-                    input_rate: VertexInputRate::Vertex,
-                }));
-                offset += size;
-            }
-
+            let vertex_layout = create_vertex_layout(attribs);
             let shader = shaders.get(attribs).unwrap_or_else(|| {
                 panic!("No Shader exists for the attributes {:?}", attribs);
             });
@@ -420,10 +388,6 @@ impl ModelRenderGraph {
                     depth_attachment_format: Some(Format::D16_UNORM),
                     ..Default::default()
                 };
-
-                let vertex_layout = VertexInputState::new()
-                    .attributes(vertex_attributes)
-                    .bindings(vertex_bindings);
 
                 GraphicsPipeline::new(
                     renderer.device.clone(),
@@ -500,14 +464,6 @@ impl ModelRenderGraph {
             set,
         }
     }
-}
-
-fn calculate_element_size(attribs: &Vec<Attribute>) -> u32 {
-    let mut total_size = 0;
-    for attrib in attribs {
-        total_size += attrib.get_size();
-    }
-    total_size
 }
 
 impl Recorder for ModelRenderGraph {
--- src/rendering/renderer_utils.rs
+++ src/rendering/renderer_utils.rs
@@ -7,6 +7,8 @@ use vulkano::format::Format;
 use vulkano::image::{Image, ImageCreateInfo, ImageType, ImageUsage};
 use vulkano::image::view::ImageView;
 use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
+use vulkano::pipeline::graphics::vertex_input::{VertexInputAttributeDescription, VertexInputBindingDescription, VertexInputRate, VertexInputState};
+use crate::io::model::{Attribute, AttributeSize};
 use crate::rendering::renderer::Renderer;
 
 pub fn load_png(png_bytes: Vec<u8>, renderer: &Renderer, uploads: &mut AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>) -> Arc<ImageView> {
@@ -51,4 +53,50 @@ pub fn load_png(png_bytes: Vec<u8>, renderer: &Renderer, uploads: &mut AutoComma
         )).unwrap();
 
     ImageView::new_default(image).unwrap()
+}
+
+pub fn create_vertex_layout(attribs: &Vec<Attribute>) -> VertexInputState {
+    let mut vertex_attributes = Vec::new();
+    let mut vertex_bindings = Vec::new();
+    let total_size = calculate_element_size(attribs);
+    let mut offset = 0;
+    for attr_idx in 0..attribs.len() {
+        let attribute = attribs.get(attr_idx).unwrap();
+        let vk_type = match attribute.size {
+            AttributeSize::None(_, _) => panic!("None attribute exists"),
+            AttributeSize::Rgba8UNorm(_, _) => Format::R8G8B8A8_UNORM,
+            AttributeSize::Rgba8Unsigned(_, _) => Format::R8G8B8A8_UINT,
+            AttributeSize::R32UInt(_, _) => Format::R32_UINT,
+            AttributeSize::R32Int(_, _) => Format::R32_SINT,
+            AttributeSize::Rgba16UNorm(_, _) => Format::R16G16B16A16_UNORM,
+            AttributeSize::Rgba16Float(_, _) => Format::R16G16B16A16_SFLOAT,
+            AttributeSize::Rg32Float(_, _) => Format::R32G32_SFLOAT,
+            AttributeSize::Rgb32Float(_, _) => Format::R32G32B32_SFLOAT,
+            AttributeSize::Rgba32Float(_, _) => Format::R32G32B32A32_SFLOAT,
+        };
+        let size = attribute.get_size();
+
+        vertex_attributes.push((attr_idx as u32, VertexInputAttributeDescription {
+            binding: 0,
+            format: vk_type,
+            offset,
+        }));
+        vertex_bindings.push((0, VertexInputBindingDescription {
+            stride: total_size,
+            input_rate: VertexInputRate::Vertex,
+        }));
+        offset += size;
+    }
+
+    VertexInputState::new()
+        .attributes(vertex_attributes)
+        .bindings(vertex_bindings)
+}
+
+fn calculate_element_size(attribs: &Vec<Attribute>) -> u32 {
+    let mut total_size = 0;
+    for attrib in attribs {
+        total_size += attrib.get_size();
+    }
+    total_size
 }
